277f45989aca104773389e84f65d6d4a885285f3
Liza <lixia101111@gmail.com>
2023-02-15 22:52:56 +0200

    Add save user function-show successfully saved message

--- src/main/java/com/mycompany/mywebapp/user/UserController.java
+++ src/main/java/com/mycompany/mywebapp/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -27,8 +28,9 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user) {
+    public String saveUser(User user, RedirectAttributes attributes) {
         userService.save(user);
+        attributes.addFlashAttribute("message", "The user has been saved!");
         return "redirect:/users";
     }
 }
