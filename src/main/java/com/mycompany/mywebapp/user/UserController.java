1c24f808d5b8aa847890cbf4385fae4098938bb1
Liza <lixia101111@gmail.com>
2023-02-15 23:27:32 +0200

    Add edit user method

--- src/main/java/com/mycompany/mywebapp/user/UserController.java
+++ src/main/java/com/mycompany/mywebapp/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,6 +25,7 @@ public class UserController {
     @GetMapping("/users/new")
     public String showNewForm(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Add New User");
         return "user_form";
     }
 
@@ -33,4 +35,20 @@ public class UserController {
         attributes.addFlashAttribute("message", "The user has been saved!");
         return "redirect:/users";
     }
+
+    @GetMapping("/users/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes attributes){
+        try {
+            User user = userService.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+            return "user_form";
+        } catch (UserNotFoundException e) {
+            attributes.addFlashAttribute("message", "The user has been saved!");
+            return "redirect:/users";
+        }
+    }
+
+
+
 }
