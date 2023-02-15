cccd48b120cf093ccd312d313593cefd6b88b3e3
Liza <lixia101111@gmail.com>
2023-02-15 09:40:42 +0200

    Add UserService class and listAll method

---
+++ src/main/java/com/mycompany/mywebapp/user/UserService.java
@@ -0,0 +1,16 @@
+package com.mycompany.mywebapp.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> listAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+}
